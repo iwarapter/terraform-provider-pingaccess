59b4209eab7f5f32230ffa7b721d5231ee1ac56e
Iain Adams <iwarapter@hotmail.com>
2020-07-07 17:27:48 +0100

    switch back to hsm provider for cross-version functional testing

--- func-tests/hsm_provider.tf
+++ func-tests/hsm_provider.tf
@@ -1,12 +1,12 @@
 resource "pingaccess_hsm_provider" "test" {
   count         = var.pa6 ? 1 : 0
-  class_name    = "com.pingidentity.pa.hsm.pkcs11.plugin.PKCS11HsmProvider"
+  class_name    = "com.pingidentity.pa.hsm.cloudhsm.plugin.AwsCloudHsmProvider"
   name          = "demo"
   configuration = <<EOF
   {
-    "slotId": "1234",
-    "library": "foo",
-    "password": "top_secret"
+    "user": "bob",
+    "password": "top_secret",
+    "partition": "p1"
   }
   EOF
 }
